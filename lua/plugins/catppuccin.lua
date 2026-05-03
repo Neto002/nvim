42874d9988f27067b76ab4308fb3aab06ef8dbed
Antonio Neto <antoniogfneto92@hotmail.com>
2026-05-03 00:21:51 -0300

    feat: adding auto complete

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -16,5 +16,18 @@ return {
 		-- fallback (garantia)
 		vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
 		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+
+		-- deixa floats transparentes (Telescope usa isso)
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+
+		-- telescope especificamente
+		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#89b4fa", bg = "none" }) -- azul catppuccin
 	end,
 }
